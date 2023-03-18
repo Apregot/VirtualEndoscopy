c0bb2518d4e451f99fc83b4dc71b288cbb835859
Daniil Verevkin <werowkin@gmail.com>
2023-03-18 22:50:29 +0200

    Настроил eslint

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,23 +1,21 @@
-import "primereact/resources/themes/lara-light-indigo/theme.css";
-import "primereact/resources/primereact.min.css";
-import "primeicons/primeicons.css";
+import 'primereact/resources/themes/lara-light-indigo/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
 
-import { useState } from 'react'
-import {Splitter, SplitterPanel} from "primereact/splitter";
-import {Menu} from "./components/menu/Menu";
+import { Splitter, SplitterPanel } from 'primereact/splitter';
+import { Menu } from './components/menu/Menu';
+import React from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <div className="App">
-        <Menu/>
-        <Splitter style={{ height: '300px' }}>
-            <SplitterPanel className="flex align-items-center justify-content-center">Panel 1</SplitterPanel>
-            <SplitterPanel className="flex align-items-center justify-content-center">Panel 2</SplitterPanel>
-        </Splitter>
-    </div>
-  )
+function App (): JSX.Element {
+    return (
+        <div className="App">
+            <Menu/>
+            <Splitter style={{ height: '300px' }}>
+                <SplitterPanel className="flex align-items-center justify-content-center">Panel 1</SplitterPanel>
+                <SplitterPanel className="flex align-items-center justify-content-center">Panel 2</SplitterPanel>
+            </Splitter>
+        </div>
+    );
 }
 
-export default App
+export default App;
