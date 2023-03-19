47873be4b26ea9caee315a442a0dd82dc85c68b2
Daniil Verevkin <werowkin@gmail.com>
2023-03-19 02:16:51 +0200

    Переворачиваем стрелочку кнопки меню

--- web/src/components/base/SelectList.tsx
+++ web/src/components/base/SelectList.tsx
@@ -1,4 +1,5 @@
 import React, { type ReactElement, type ReactNode } from 'react';
+import styles from './SelectList.module.scss';
 import { ReactComponent as Arrow } from './arrow.svg';
 interface TProps {
     children: ReactNode
@@ -7,9 +8,10 @@ interface TProps {
 export const SelectList = (props: TProps): ReactElement => {
     return (
         <div>
-            <button className="px-2 py-0.5 rounded-md hover:bg-neutral-100 transition-colors">{props.children}</button>
-            <Arrow/>
-            <ul>
+            <button className={`px-2 py-0.5 rounded-md hover:bg-neutral-100 transition-colors ${styles.button}`}>
+                <span className="mr-1.5">{props.children}</span><Arrow className={styles.arrow}/>
+            </button>
+            <ul className={styles.list}>
                 <li>1</li>
                 <li>2</li>
             </ul>
