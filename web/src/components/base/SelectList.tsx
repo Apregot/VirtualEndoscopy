ac1ca13af1d12b7fafc0d41744230569d6cebc71
Daniil Verevkin <werowkin@gmail.com>
2023-03-19 03:06:51 +0200

    Скрываем меню если произошло нажатие вне контекста меню

--- web/src/components/base/SelectList.tsx
+++ web/src/components/base/SelectList.tsx
@@ -1,4 +1,4 @@
-import React, { type ReactElement, type ReactNode, useState } from 'react';
+import React, { type ReactElement, type ReactNode, useEffect, useState } from 'react';
 import styles from './SelectList.module.scss';
 import { ReactComponent as Arrow } from './arrow.svg';
 interface TProps {
@@ -14,6 +14,17 @@ export const SelectList = (props: TProps): ReactElement => {
 
     const listDisplay = opened ? 'block' : 'none';
 
+    useEffect(() => {
+        if (opened) {
+            setTimeout(() => {
+                // HACK: При нажатии на другие кнопки нужно скрывать чужие меню. Для реализации оного в голову пришла только идея с таймером
+                document.addEventListener('click', () => {
+                    setOpen(false);
+                }, { once: true });
+            }, 0);
+        }
+    }, [opened]);
+
     return (
         <div>
             <button
@@ -22,7 +33,10 @@ export const SelectList = (props: TProps): ReactElement => {
             >
                 <span className="mr-1.5">{props.children}</span><Arrow className={styles.arrow}/>
             </button>
-            <ul className={styles.list} style={{ display: listDisplay }}>
+            <ul
+                onClick={(e) => { e.stopPropagation(); }}
+                className={styles.list} style={{ display: listDisplay }}
+            >
                 <li>1</li>
                 <li>2</li>
             </ul>
