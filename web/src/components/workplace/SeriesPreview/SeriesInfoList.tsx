b9d5017fe3e9c2d08c892b9539766aeec0ca1dc8
Daniil Verevkin <werowkin@gmail.com>
2023-04-09 20:57:25 +0200

    Изменил фавиконку приложения

--- web/src/components/workplace/SeriesPreview/SeriesInfoList.tsx
+++ web/src/components/workplace/SeriesPreview/SeriesInfoList.tsx
@@ -1,33 +1,33 @@
-import React, { type ReactElement } from "react";
-import Tab from "react-bootstrap/Tab";
-import { SeriesInfo } from "./SeriesInfo";
-import Tabs from "react-bootstrap/Tabs";
-import { useAppSelector } from "../../../hooks/redux";
-import { type Series } from "../../../lib/Series";
+import React, { type ReactElement } from 'react';
+import Tab from 'react-bootstrap/Tab';
+import { SeriesInfo } from './SeriesInfo';
+import Tabs from 'react-bootstrap/Tabs';
+import { useAppSelector } from '../../../hooks/redux';
+import { type Series } from '../../../lib/Series';
 
 export const SeriesInfoList = (): ReactElement => {
-  const { patientsSeriesList } = useAppSelector((state) => state.seriesReducer);
-  return (
-    <Tabs
-      defaultActiveKey="unsorted"
-      id="uncontrolled-tab-example"
-      className="bg-neutral-50"
-    >
-      <Tab eventKey="unsorted" title="Несортированные изображения">
+    const { patientsSeriesList } = useAppSelector((state) => state.seriesReducer);
+    return (
+        <Tabs
+            defaultActiveKey="unsorted"
+            id="uncontrolled-tab-example"
+            className="bg-neutral-50"
+        >
+            <Tab eventKey="unsorted" title="Несортированные изображения">
         unsorted
-      </Tab>
-      {patientsSeriesList?.map((seriesListItem: Array<Series>) => {
-        const seriesItem = seriesListItem.at(0);
-        return (
-          <Tab
-            key={seriesItem?.getId()}
-            eventKey={seriesItem?.getId()}
-            title={seriesItem?.getPatient().name}
-          >
-            <SeriesInfo series={seriesListItem} />
-          </Tab>
-        );
-      })}
-    </Tabs>
-  );
+            </Tab>
+            {patientsSeriesList?.map((seriesListItem: Series[]) => {
+                const seriesItem = seriesListItem.at(0);
+                return (
+                    <Tab
+                        key={seriesItem?.getId()}
+                        eventKey={seriesItem?.getId()}
+                        title={seriesItem?.getPatient().name}
+                    >
+                        <SeriesInfo series={seriesListItem} />
+                    </Tab>
+                );
+            })}
+        </Tabs>
+    );
 };
--- web/src/store/reducers/SeriesSlice.ts
+++ web/src/store/reducers/SeriesSlice.ts
@@ -1,30 +1,30 @@
-import { Series } from "../../lib/Series";
-import { createSlice, type PayloadAction } from "@reduxjs/toolkit";
+import { type Series } from '../../lib/Series';
+import { createSlice, type PayloadAction } from '@reduxjs/toolkit';
 
 interface SeriesState {
-  patientsSeriesList: Array<Series[]>;
+    patientsSeriesList: Series[][]
 }
 
 const initialState: SeriesState = {
-  patientsSeriesList: [],
+    patientsSeriesList: []
 };
 
 export const seriesSlice = createSlice({
-  name: "patientsSeriesList",
-  initialState,
-  reducers: {
-    pushSeries: (state, action: PayloadAction<Series>) => {
-      let check = true;
+    name: 'patientsSeriesList',
+    initialState,
+    reducers: {
+        pushSeries: (state, action: PayloadAction<Series>) => {
+            let check = true;
 
-      for (const element of state.patientsSeriesList) {
-        if (element.at(0)?.getId == action.payload.getId) {
-            check = false;
-            element.push(action.payload);
-          }
-      }
-      check && state.patientsSeriesList.push([action.payload]);
-    },
-  },
+            for (const element of state.patientsSeriesList) {
+                if (element.at(0)?.getId === action.payload.getId) {
+                    check = false;
+                    element.push(action.payload);
+                }
+            }
+            check && state.patientsSeriesList.push([action.payload]);
+        }
+    }
 });
 
 export default seriesSlice.reducer;
