46e68d59efc5400b76e03968714e8c82245dae25
Daniil Verevkin <werowkin@gmail.com>
2023-04-11 00:28:02 +0200

    Вывели первый 3D объект в фрейме

--- web/src/components/workplace/QuadView/QuadView.tsx
+++ web/src/components/workplace/QuadView/QuadView.tsx
@@ -1,4 +1,4 @@
-import React, { type ReactElement } from 'react';
+import React, { type ReactElement, useEffect, useRef } from 'react';
 import * as THREE from 'three';
 import { useAppSelector } from '../../../hooks/redux';
 import styles from './QuadView.module.scss';
@@ -17,9 +17,40 @@ export const QuadView = (): ReactElement => {
         const RIO = new THREE.Box3(new THREE.Vector3(0, 0, 0), seriesStack.dimensionsIJK.clone());
         console.log(RIO);
     }
+    const canvasRef = useRef(null);
+
+    useEffect(() => {
+        if (canvasRef.current === null) {
+            return;
+        }
+
+        const scene = new THREE.Scene();
+        const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+        const renderer = new THREE.WebGLRenderer({ canvas: canvasRef.current });
+
+        const geometry = new THREE.PlaneGeometry(5, 5);
+        const material = new THREE.MeshBasicMaterial({ color: 0xffffff, side: THREE.DoubleSide });
+        const plane = new THREE.Mesh(geometry, material);
+        scene.add(plane);
+
+        camera.position.z = 5;
+
+        function animate(): void {
+            requestAnimationFrame(animate);
+
+            // Добавляем вращение плоскости
+            plane.rotation.x += 0.01;
+            plane.rotation.y += 0.01;
+
+            renderer.render(scene, camera);
+        }
+
+        animate();
+    }, [canvasRef]);
+
     return (
         <div className={styles.wrapper}>
-            <div className={`${styles.renderer} ${styles.red}`}></div>
+            <div className={`${styles.renderer} ${styles.red}`}><canvas style={{ width: '100%', height: '100%' }} ref={canvasRef} /></div>
             <div className={`${styles.renderer} ${styles.blue}`}></div>
             <div className={`${styles.renderer} ${styles.yellow}`}></div>
             <div className={`${styles.renderer} ${styles.green}`}></div>
