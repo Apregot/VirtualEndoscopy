e0bee8d9bc146463583069200a26074511c9b7c7
Dmitrii Sobinin <Sobalchik@yandex.ru>
2023-04-20 09:01:44 +0200

    Тестовое подключение

--- web/src/components/menu/ConnectionInfo.tsx
+++ web/src/components/menu/ConnectionInfo.tsx
@@ -1,13 +1,25 @@
-import React, { type ReactElement } from 'react';
+import React, { useEffect, type ReactElement, useState } from 'react';
 
 export enum ConnectionStatus {
     CONNECTED,
     DISCONNECTED,
     PROGRESS,
 };
-
 export const ConnectionInfo = (): ReactElement => {
-    const status = ConnectionStatus.DISCONNECTED;
+    const [status, setStatus] = useState(ConnectionStatus.DISCONNECTED);
+
+    useEffect(() => {
+        const socket = new WebSocket('ws:127.0.0.1:81', 'ffr-protocol');
+        socket.onopen = function(e) {
+            alert('CONNECTED');
+            setStatus(ConnectionStatus.CONNECTED);
+        };
+        socket.onclose = function(e) {
+            alert('DISCONNECTED');
+            setStatus(ConnectionStatus.DISCONNECTED);
+        };
+    }, []);
+    
     const classes = 'ml-3 mt-1 max-h-6 px-2 py-1 rounded-lg font-semibold text-xs';
     
     return (
@@ -17,24 +29,24 @@ export const ConnectionInfo = (): ReactElement => {
 
 function getStatusClasses(status: ConnectionStatus): string {
     switch (status) {
-        case ConnectionStatus.CONNECTED:
-            return 'text-green-600 bg-green-400/30';
-        case ConnectionStatus.DISCONNECTED:
-            return 'text-red-600 bg-red-400/30';
-        case ConnectionStatus.PROGRESS:
-            return 'text-yellow-600 bg-yellow-400/30';
+    case ConnectionStatus.CONNECTED:
+        return 'text-green-600 bg-green-400/30';
+    case ConnectionStatus.DISCONNECTED:
+        return 'text-red-600 bg-red-400/30';
+    case ConnectionStatus.PROGRESS:
+        return 'text-yellow-600 bg-yellow-400/30';
     }
     return '';
 }
 
 function getStatusTitle(status: ConnectionStatus): string {
     switch (status) {
-        case ConnectionStatus.CONNECTED:
-            return 'Connected';
-        case ConnectionStatus.DISCONNECTED:
-            return 'Disconnected';
-        case ConnectionStatus.PROGRESS:
-            return 'Connection...';
+    case ConnectionStatus.CONNECTED:
+        return 'Connected';
+    case ConnectionStatus.DISCONNECTED:
+        return 'Disconnected';
+    case ConnectionStatus.PROGRESS:
+        return 'Connection...';
     }
     return 'Undefined';
 }
