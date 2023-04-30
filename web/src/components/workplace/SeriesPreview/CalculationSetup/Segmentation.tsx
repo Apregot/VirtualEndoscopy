586600984bb40671d0e1e270c16245615adfccfe
nessai1 <werowkin@gmail.com>
2023-05-01 01:12:15 +0200

    Попап + Прогрессбар загрузки DICOM файла на сервер (пока только верстка)

--- web/src/components/workplace/SeriesPreview/CalculationSetup/Segmentation.tsx
+++ web/src/components/workplace/SeriesPreview/CalculationSetup/Segmentation.tsx
@@ -1,14 +1,29 @@
-import React, { type ReactElement } from 'react';
+import React, { type ReactElement, useState } from 'react';
 import { BaseButton } from '../../../base/Button';
 import { useAppSelector } from '../../../../hooks/redux';
+import { Popup } from '../../../base/Popup';
+import ProgressBar from 'react-bootstrap/ProgressBar';
 
 export const Segmentation = (): ReactElement => {
     const { selectedPreviewSeries } = useAppSelector((state) => state.patientsSeriesList);
+    const [loader, setLoader] = useState(false);
 
     const disabled = selectedPreviewSeries === null;
     return (
         <div>
-            <BaseButton disabled={disabled} onClick={() => { console.log('initializing ffr...'); }}>Initialize FFR processing</BaseButton>
+            {
+                loader
+                    ? (
+                        <Popup onClose={() => { setLoader(false); }} title={'Transfering DICOM data'} show={true}>
+                            <div style={{ paddingBottom: '20px' }}>
+                        Загрузка DICOM...
+                                <ProgressBar animated now={60} label={'60%'} />
+                            </div>
+                        </Popup>
+                    )
+                    : ''
+            }
+            <BaseButton disabled={disabled} onClick={() => { console.log('initializing ffr...'); setLoader(true); }}>Initialize FFR processing</BaseButton>
         </div>
     );
 };
