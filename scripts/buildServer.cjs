c6c0c90f768737df2dceefef96638eec1a886815
Melnikov Savva <msavva@bk.ru>
2023-05-06 20:57:39 +0200

    Попытка починичть сборку сервера в github actions

--- scripts/buildServer.cjs
+++ scripts/buildServer.cjs
@@ -3,11 +3,13 @@ const { exec } = require('child_process');
 
 const execCallback = (err, stdout, stderr) => {
 	if (err) {
-		console.log(`error: ${stderr}`);
-		return;
+		throw new Error(`error: ${stderr}`);
 	}
 	console.log(`out: ${stdout}`);
 }
 
-const buildDir = path.normalize(__dirname +  '"\\..\\builtServer"') + '\\';
-exec('go build -C .\\server\\cmd\\main -o ' + buildDir, execCallback);
+const buildDir = path.normalize(__dirname +  "\\..\\builtServer") + "\\";
+console.log('buildDir: ' + buildDir);
+const command = "go build -C .\\server\\cmd\\main -o " + buildDir;
+console.log('command: ' + command);
+exec(command, execCallback);
